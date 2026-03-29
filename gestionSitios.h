5374b133bedddfe49abfc48d9387f763e61aa406
Divan27 <49537341+Divan27@users.noreply.github.com>
2026-03-29 04:54:35 -0600

    Ajuste de orden
    Added struct definition for Sitio and global variables.

--- gestionSitios.h
+++ gestionSitios.h
@@ -1,9 +1,20 @@
 #ifndef GESTION_SITIOS_H
 #define GESTION_SITIOS_H
 
+// DEFINICIÓN DEL STRUCT 
+typedef struct {
+    char nombre[100];
+    char ubicacion[100];
+    char web[100];
+} Sitio;
+
+// variables globales con extern
+extern Sitio **sitios;
+extern int cantidadSitios;
+
+// funciones
 void gestionSitios();
 void cargarSitiosLote();
 void listarSitios();
-int existeID(int id);
 
 #endif
